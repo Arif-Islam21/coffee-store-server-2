1bfb39fe51e6bc36041eceef18cf0a79fbd0332a
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-08-14 16:15:16 +0600

    working on add mango

--- coffee-store-client/src/Components/AddCoffee.jsx
+++ coffee-store-client/src/Components/AddCoffee.jsx
@@ -1,5 +1,33 @@
 const AddCoffee = () => {
-  return <div>add coffee</div>;
+  return (
+    <div>
+      <h2>Add Coffee</h2>
+      <form>
+        <div className="flex justify-around">
+          <label className="form-control w-full max-w-lg">
+            <div className="label">
+              <span className="label-text">Coffee name?</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Coffee Name"
+              className="input input-bordered w-full max-w-lg"
+            />
+          </label>
+          <label className="form-control w-full max-w-lg">
+            <div className="label">
+              <span className="label-text">Coffee Quantity?</span>
+            </div>
+            <input
+              type="number"
+              placeholder="Coffee Quantity"
+              className="input input-bordered w-full max-w-lg"
+            />
+          </label>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddCoffee;
