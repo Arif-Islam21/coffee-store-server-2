f1162d8da6da3d03dd647f4a455e63b584a4ccd7
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-08-14 15:41:30 +0600

    first commit

---
+++ coffee-store-client/src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AddCoffee from "./Components/AddCoffee.jsx";
+import UpdateCoffee from "./Components/UpdateCoffee.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path: "addCoffee",
+    element: <AddCoffee></AddCoffee>,
+  },
+  {
+    path: "updateCoffee",
+    element: <UpdateCoffee></UpdateCoffee>,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
