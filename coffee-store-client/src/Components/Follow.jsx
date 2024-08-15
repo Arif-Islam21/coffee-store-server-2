fc52489a99bd00133daffc07263d9539f2bd9d41
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-08-15 16:11:29 +0600

    social section done

--- coffee-store-client/src/Components/Follow.jsx
+++ coffee-store-client/src/Components/Follow.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import img from "../../public/1912.i211.035..coffee cup on table realistic.jpg";
 
 const Follow = () => {
   return (
@@ -8,6 +8,16 @@ const Follow = () => {
         <h1 className="font-rancho text-5xl my-4 text-[#331A15] font-bold drop-shadow-sm blur-[1px]">
           Follow on Instagram
         </h1>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 px-8 mb-8">
+        <img className="h-80 w-96 rounded-xl" src={img} alt="" />
+        <img className="h-80 w-96 rounded-xl" src={img} alt="" />
+        <img className="h-80 w-96 rounded-xl" src={img} alt="" />
+        <img className="h-80 w-96 rounded-xl" src={img} alt="" />
+        <img className="h-80 w-96 rounded-xl" src={img} alt="" />
+        <img className="h-80 w-96 rounded-xl" src={img} alt="" />
+        <img className="h-80 w-96 rounded-xl" src={img} alt="" />
+        <img className="h-80 w-96 rounded-xl" src={img} alt="" />
       </div>
     </div>
   );
