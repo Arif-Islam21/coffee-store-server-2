fc8942790f118a903b90c2f2c42f9f6afbbf48ae
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-08-15 16:22:32 +0600

    footer set on the bottom

--- coffee-store-client/src/App.jsx
+++ coffee-store-client/src/App.jsx
@@ -6,7 +6,9 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
-      <Outlet></Outlet>
+      <div className="min-h-[calc(100vh-64px)]">
+        <Outlet></Outlet>
+      </div>
       <Footer></Footer>
     </>
   );
