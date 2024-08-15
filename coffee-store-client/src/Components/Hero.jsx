b53a50ffb3eb4c48cc5519d6d221211dd7407b79
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-08-15 10:06:16 +0600

    hero section done

---
+++ coffee-store-client/src/Components/Hero.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div
+      className="hero  min-h-[80vh]"
+      style={{
+        backgroundImage:
+          "url(https://img.freepik.com/free-photo/fresh-coffee-steams-wooden-table-close-up-generative-ai_188544-8923.jpg)",
+      }}
+    >
+      {/* <div className="hero-overlay"></div> */}
+      <div className="hero-content hover:hero-overlay hover:bg-opacity-30 text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-3xl font-bold font-rancho">
+            Would you like a Cup of Delicious Coffee?
+          </h1>
+          <p className="mb-5 font-raleway">
+            It's coffee time - Sip & Savor - Relaxation in every sip! Get the
+            nostalgia back!! Your companion of every moment!!! Enjoy the
+            beautiful moments and make them memorable.
+          </p>
+          <button className="btn bg-[#E3B577] font-bold">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- coffee-store-client/src/Components/Homepage.jsx
+++ coffee-store-client/src/Components/Homepage.jsx
@@ -1,7 +1,9 @@
+import Hero from "./Hero";
+
 const Homepage = () => {
   return (
     <div>
-      <h2>This is home page</h2>
+      <Hero></Hero>
     </div>
   );
 };
