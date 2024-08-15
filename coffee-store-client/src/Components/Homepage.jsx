4092d981243ebebbdc5e28479b89b2d3f9fcfed5
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-08-15 15:36:15 +0600

    working on popular product section

--- coffee-store-client/src/Components/Homepage.jsx
+++ coffee-store-client/src/Components/Homepage.jsx
@@ -1,11 +1,13 @@
 import Hero from "./Hero";
 import HeroBase from "./HeroBase";
+import PopularProduct from "./PopularProduct";
 
 const Homepage = () => {
   return (
     <div>
       <Hero></Hero>
       <HeroBase></HeroBase>
+      <PopularProduct></PopularProduct>
     </div>
   );
 };
---
+++ coffee-store-client/src/Components/PopularProduct.jsx
@@ -0,0 +1,24 @@
+import { BiSolidCoffee } from "react-icons/bi";
+
+const PopularProduct = () => {
+  return (
+    <div>
+      <div className="flex flex-col justify-center items-center my-6">
+        <h2 className="font-raleway font-light text-2xl">
+          --- Sip & Savor ---
+        </h2>
+        <h1 className="font-rancho text-5xl my-4 text-[#331A15] font-bold drop-shadow-sm blur-[1px]">
+          Our Popular Products
+        </h1>
+        <button className="flex justify-between bg-[#E3B577] my-4 items-center btn border-2 border-[#331A15]">
+          <span className="font-bold text-xl text-white font-rancho">
+            Add Coffee
+          </span>
+          <BiSolidCoffee className="text-[#331A15] font-bold text-xl" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PopularProduct;
