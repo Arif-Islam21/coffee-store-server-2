e1e64dd8841c2671783841a85245f0d76c0db20d
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-08-17 09:24:19 +0600

    working with authentication

---
+++ coffee-store-client/src/FIrebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDzmkgpfCcOZyr3hQH_5_nxxGTHs9QlM8",
+  authDomain: "coffee-store-recap-25a34.firebaseapp.com",
+  projectId: "coffee-store-recap-25a34",
+  storageBucket: "coffee-store-recap-25a34.appspot.com",
+  messagingSenderId: "580979269891",
+  appId: "1:580979269891:web:b4371f9193bfd1e9f3c936",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
